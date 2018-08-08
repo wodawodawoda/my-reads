8be2e30e842bad5863dbfa0aae5600e433ae38df
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-08-08 02:12:03 +0200

    Add camelcase to text feature

--- src/modules/Shelf/Shelf.js
+++ src/modules/Shelf/Shelf.js
@@ -3,7 +3,12 @@ import './Shelf.css'
 
 const Shelf = ({ name, children }) => (
 	<section className="shelf">
-		<h2 className="shelf__name">{name}</h2>
+		<h2 className="shelf__name">
+			{name
+				.replace(/([A-Z])/g, ' $1')
+				.replace(/./, firstLetter => firstLetter.toUpperCase())
+			}
+		</h2>
 		<ul className="shelf__books">
 			{children}
 		</ul>
